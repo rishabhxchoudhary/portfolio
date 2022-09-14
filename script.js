4032d311020f8a40620e1a4c63eda29309a41544
Rishabh Kumar <97455703+rk26072003@users.noreply.github.com>
2022-09-14 20:39:11 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('.main');
+const loader = document.querySelector('.loader');
+
+window.onload = function(){
+    loader.style.animation="";
+    loader.style.display="none";
+    main.style.display="initial";
+    };
