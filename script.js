ad57b42a030cbd5f61a54fb516f9680ad51ab8d5
Rishabh Kumar <97455703+rk26072003@users.noreply.github.com>
2022-09-15 19:30:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const main = document.querySelector('.main');
 const loader = document.querySelector('.loader');
+const p=document.getElementById("rk");
 
 window.onload = function(){
     loader.style.animation="";
     loader.style.display="none";
+    p.style.animation="typing 3s steps(20),blink-caret .75s step-end infinite";
     // main.style.display="initial";
 };
