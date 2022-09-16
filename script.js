4eb6b787dcedfb80aa69455cf4e3982831902b7c
Rishabh Kumar <97455703+rk26072003@users.noreply.github.com>
2022-09-16 20:14:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const main = document.querySelector('.main');
 const loader = document.querySelector('.loader');
 const p=document.getElementById("rk");
 
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+// setTimeout(() => {  console.log("World!"); }, 5000);
 window.onload = function(){
-    loader.style.animation="";
-    loader.style.display="none";
-    p.style.animation="typing 3s steps(20),blink-caret .75s step-end infinite";
-    // main.style.display="initial";
+    // sleep(5000).then(()=>{
+        loader.style.display="none";
+        p.style.animation="typing 3s steps(20),blink-caret .75s step-end infinite";
+        main.style.overflow="scroll";
+    // })
+    
 };
