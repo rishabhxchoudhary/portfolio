013ecbc2ff5e840d103c9e21cd0a81f3092a4648
Rishabh Kumar <97455703+rk26072003@users.noreply.github.com>
2022-09-18 20:15:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const main = document.querySelector('.main');
 const loader = document.querySelector('.loader');
 const p=document.getElementById("rk");
+const hero = document.querySelector('.hero');
+const about = document.querySelector('.about');
+const education = document.querySelector('.education');
+const thanks = document.querySelector('.thanks');
 
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
@@ -15,3 +19,26 @@ window.onload = function(){
     })
     
 };
+
+main.addEventListener('scroll', function(){
+    var value = main.scrollTop;
+    console.log(value);
+    if (value>0 && value<350){
+        let y = -value/350 + 1;
+        hero.style.opacity=`${(y)}`;    
+    }
+    else if (value>=734 && value <1133){
+        let y = -value/4 + 300;
+        about.style.opacity=`${(y/100)}`;  
+    }
+    else if (value>=1468 && value <=2202){
+        let y = -value/4 + 500;
+        education.style.opacity=`${(y/100)}`;  
+
+        let y1 = value/734 - (1468/734);
+        thanks.style.opacity=`${(y1)}`;  
+    }
+
+    
+
+})
